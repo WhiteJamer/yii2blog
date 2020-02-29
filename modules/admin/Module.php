874c28e2e099d99df2ec7707de15651e954bc3dc
whitejamer <mihailkuzin.job@yandex.ru>
2020-02-29 18:12:10 +0300

    Restricted access to admin-panel

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -2,6 +2,9 @@
 
 namespace app\modules\admin;
 
+use yii\filters\AccessControl;
+use yii\web\NotFoundHttpException;
+
 /**
  * admin module definition class
  */
@@ -21,5 +24,26 @@ class Module extends \yii\base\Module
         parent::init();
 
         // custom initialization code goes here
+    }
+
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'denyCallback' => function($role, $action)
+                {
+                    throw new NotFoundHttpException();
+                },
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'matchCallback' => function($role, $action)
+                        {
+                            return \Yii::$app->user->identity->isAdmin;
+                        }
+                    ]
+                ]
+            ]
+        ];
     }
 }
